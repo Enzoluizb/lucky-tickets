835035b2753e684b4c466ed0d4b5a218fafd5b69
enzoluizb <enzoluiz.brt@gmail.com>
2023-01-13 13:53:38 -0300

    primeiro commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link href="https://fonts.googleapis.com/css2?family=ROBOTO:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;0,100;1,300;1,400;1,500;1,700;1,900&display==sw" rel="stylesheet"/> 
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
